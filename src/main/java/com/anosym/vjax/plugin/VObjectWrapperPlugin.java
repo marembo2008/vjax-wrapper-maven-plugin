d0e1fa906dc2c5588760f2bcf13d7356dc3e7386
Marembo Ochieng <marembo2008@gmail.com>
2014-12-20 11:07:14 +0100

    update with vjax reflection

--- src/main/java/com/anosym/vjax/plugin/VObjectWrapperPlugin.java
+++ src/main/java/com/anosym/vjax/plugin/VObjectWrapperPlugin.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.anosym.vjax.plugin;
 
 import com.anosym.vjax.v3.wrapper.VObjectWrapper;
@@ -28,49 +23,48 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 @Mojo(name = "vjax", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class VObjectWrapperPlugin extends AbstractMojo {
 
-  /**
-   * Fully qualified name of the source directory for the generated sources. There is no default.
-   * The final name will have generated-sourcs/vjax-wrapper appended based on maven source dir structure and the value
-   * of {@link #sourceDirType}
-   */
-  @Parameter(required = true)
-  private File sourceDir;
-  @Parameter(property = "project.runtimeClasspathElements", required = true, readonly = true)
-  private List<String> classpath;
-  /**
-   * Options include main, test, generated-sources or any other user specified directory.
-   */
-  @Parameter(defaultValue = "generated-sources")
-  private String sourceDirType;
+    /**
+     * Fully qualified name of the source directory for the generated sources. There is no default.
+     * The final name will have generated-sources/vjax-wrapper appended based on maven source dir structure and the value
+     * of {@link #sourceDirType}
+     */
+    @Parameter(required = true)
+    private File sourceDir;
+    @Parameter(property = "project.runtimeClasspathElements", required = true, readonly = true)
+    private List<String> classpath;
+    /**
+     * Options include main, test, generated-sources or any other user specified directory.
+     */
+    @Parameter(defaultValue = "generated-sources")
+    private String sourceDirType;
 
-  public void execute() throws MojoExecutionException, MojoFailureException {
-    if (sourceDir == null) {
-      throw new MojoFailureException("Source Dir is required");
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (sourceDir == null) {
+            throw new MojoFailureException("Source Dir is required");
+        }
+        try {
+            init();
+            File path = new File(sourceDir, sourceDirType + "/vjax-wrapper");
+            System.out.println("Generate sources directory...." + path.getAbsolutePath());
+            VObjectWrapper vow = new VObjectWrapper(path.getAbsolutePath());
+            vow.process();
+        } catch (Exception ex) {
+            throw new MojoExecutionException("Error generating sources", ex);
+        }
     }
-    try {
-      init();
-      File path = new File(sourceDir, sourceDirType + "/vjax-wrapper");
-      System.out.println("Generate sources directory...." + path.getAbsolutePath());
-      VObjectWrapper vow = new VObjectWrapper(path.getAbsolutePath());
-      vow.init();
-      vow.process();
-    } catch (Exception ex) {
-      throw new MojoExecutionException("Error generating sources", ex);
-    }
-  }
 
-  private void init() {
-    try {
-      Set<URL> urls = new HashSet<URL>();
-      for (String element : classpath) {
-        urls.add(new File(element).toURI().toURL());
-      }
-      ClassLoader contextClassLoader = URLClassLoader.newInstance(
-              urls.toArray(new URL[0]),
-              Thread.currentThread().getContextClassLoader());
-      Thread.currentThread().setContextClassLoader(contextClassLoader);
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
+    private void init() {
+        try {
+            Set<URL> urls = new HashSet<URL>();
+            for (String element : classpath) {
+                urls.add(new File(element).toURI().toURL());
+            }
+            ClassLoader contextClassLoader = URLClassLoader.newInstance(
+                    urls.toArray(new URL[0]),
+                    Thread.currentThread().getContextClassLoader());
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
